bb38d5c7568e63a5145d83d2ba57e433cf0c8ed7
yrsa <yrsa.lifvergren@gmail.com>
2018-10-03 08:47:48 +0200

    read csv-file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    cout << "Hello world!" << endl;
+    ifstream file ( "LIU1_2018-02-19.log_sc.csv" );		//timestamp, event, value, duration of event
+	string line;
+
+	if (file.is_open())
+  	{
+	    while ( getline (file,line,',') )	//read one line, separate when there is a ','
+	    {
+	    	//save into the class Event (skip first line?)
+	      	cout << line << '\n';
+	    }
+	    file.close();
+	}
 
     return 0;
 }
